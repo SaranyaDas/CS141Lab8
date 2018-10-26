bb1dc0318fd5d52c1863c4488500893d8d003b5a
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-27 00:49:59 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,60 +1,37 @@
-
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-//function for kth largest
-int kLargest(int arr[], int x, int k){
-	int temp;
-	for(int i=x-1; i>0; i--){
-		for(int j=0; j<i; j++){
-			if(arr[j]>arr[j+1]){
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-	return arr[x-k];
+int klarge(int a[],int s,int n)
+{
+	return a[s-n];
 }
-
-//function for kth smallest
-int kSmallest(int arr[], int x, int k){
-	int temp;
-	for(int i=x-1; i>0; i--){
-		for(int j=0; j<i; j++){
-			if(arr[j]>arr[j+1]){
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-	return arr[k-1];
+int ksmall(int a[],int s,int n)
+{
+	return a[n-1];
 }
-
-int main(){
-
-	//input for size of array
-	int s,k;
-	cout << "Enter the size of your array : " << endl;
-	cin >> s;
-	
-	//define array
-	int Array1[s];
-
-	//input for elements
-	cout << "Enter the elements of your array : " << endl;
-	for(int a=0;a<s;a++){
-		cin >> Array1[a];
-	}
-
-	//input for 'k'
-	cout << "Enter 'k' for the kth largest or the kth smallest element of your array : " << endl;
-	cin >> k;
-
-	//print results by calling the functions
-	cout << k << "th largest element of your array is : " << kLargest(Array1,s,k) << endl;
-	cout << k << "th smallest element of your array is : " << kSmallest(Array1,s,k) << endl;
-
+int main()
+{
+	int size=0;
+	cout<<"Enter the size of the array."<<endl;
+	cin>>size;
+	int arr[size];
+	cout<<"Enter the elements of the array."<<endl;
+	for(int i=0;i<size;i++)
+	cin>>arr[i];
+	int temp=0;
+	for (int i=0;i<size-1;i++)    
+        for (int j=0;j<size-i-1;j++)  
+		if(arr[j]>arr[j+1]) 
+		{
+			arr[j]=arr[j]+arr[j+1];
+			arr[j+1]=arr[j]-arr[j+1];
+			arr[j]=arr[j]-arr[j+1];
+		} 
+	int k1,k2;
+	cout<<"Enter the nth largest element to be found."<<endl;
+	cin>>k1;
+	cout<<"Enter the nth smallest element to be found."<<endl;
+	cin>>k2;
+	cout<<"The kth largest element in the array is "<<klarge(arr,size,k1)<<endl;
+	cout<<"The kth smallest elememt in the array is "<<ksmall(arr,size,k2)<<endl;
 	return 0;
 }
