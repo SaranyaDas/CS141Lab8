d96f5d50e06d9e5445e1599788f2f67f92fc5aa6
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-26 23:48:45 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,23 @@
-
+//To find sum of all elements of an array using functions
+#include <iostream>
+using namespace std;
+//sum function
+int arraysum(int array[], int n) {
+	int sum = 0;
+	for (int i; i < n; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+int main() {
+	int n;
+	cout << "Enter size of array : ";
+	cin >> n;
+	int array[n];
+	cout << "Enter elements to be included in array: " <<;
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
+	}
+	cout << "Sum of elements in array is: " << arraysum(array, n) << endl;
+	return 0;	
+}
